beb0904431a425f7b000524fc83f8854ed560d53
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-20 21:12:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,33 @@ app = Flask(__name__)
 
 # create a list the 8 image file names (sch logo, 4 house mascots, stem cca, guitar ensemble, taekwondo)
 # will refer to the colours flask app from old computing task to style the table
+
 answer_table = [] # table of the names of image files
 game_table = [] # table with 4 rows 4 cols of None data; to be processed by jinja during game
+tries = 0 # number of attempts at guessing
 
 @app.route("/")
 def lobby():
   return render_template("lobby.html")
 
+@app.route("/game")
+def game():
+  global game_table
+  return render_template("game.html")
+
 @app.route("/prep_game")
 def prep_game():
   # extend image names list by itself and shuffle
   # fill the list contents into 2d array for table display later
-  return render_template("game.html")
+  return redirect("/game")
+
+@app.route("/process_game")
+def process_game():
+  # check for duplicate guesses
+  # check if both guesses are the same image names from answer_table
+  # update game_table with the image names if guessed correctly
+  # jinja message 
+  return redirect("/game")
 
 if __name__ == "__main__":
   app.run(debug=True, port=7777)
