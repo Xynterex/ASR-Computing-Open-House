d479269c06ffeb91f6245efb9d258582d68b4eda
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-20 19:26:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 # create a list the 8 image files (sch logo, 4 house mascots, stem cca, guitar ensemble, taekwondo)
 # extend list by itself and shuffle
 # fill the list contents into 2d array for table display later
+# will refer to the colours flask app from old computing task to style the table
 
 @app.route("/")
 def lobby():
