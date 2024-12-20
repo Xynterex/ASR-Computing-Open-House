2ecadd661cab9936f1ec117931dcfafe1af46b9b
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-20 19:15:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+# create a list the 8 image files (sch logo, 4 house mascots, stem cca, guitar ensemble, taekwondo)
+# extend list by itself and shuffle
+# fill the list contents into 2d array for table display later
+
 @app.route("/")
 def lobby():
   return render_template("lobby.html")
