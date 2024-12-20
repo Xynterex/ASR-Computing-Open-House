17cabe16949501b28f70da3d876e244f7f77c712
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-20 20:58:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-# create a list the 8 image files (sch logo, 4 house mascots, stem cca, guitar ensemble, taekwondo)
-# extend list by itself and shuffle
-# fill the list contents into 2d array for table display later
+# create a list the 8 image file names (sch logo, 4 house mascots, stem cca, guitar ensemble, taekwondo)
 # will refer to the colours flask app from old computing task to style the table
+table = []
 
 @app.route("/")
 def lobby():
   return render_template("lobby.html")
+
+@app.route("/prep_game")
+def prep_game():
+  # extend image names list by itself and shuffle
+  # fill the list contents into 2d array for table display later
+  return render_template("game.html")
 
 if __name__ == "__main__":
   app.run(debug=True, port=7777)
