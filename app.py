81aad00cd589e9bf280c257c92d6aad32180337d
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-20 19:01:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template, url_for, request, redirect
 
+app = Flask(__name__)
+
+@app.route("/")
+def lobby():
+  return render_template("lobby.html")
+
+if __name__ == "__main__":
+  app.run(debug=True, port=7777)
