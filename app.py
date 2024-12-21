cd185a24038a196b4c235d29fb7e3fa06150f1c7
Xynterex <158581153+Xynterex@users.noreply.github.com>
2024-12-21 11:32:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def prep_game():
     # fill the list contents into answer_table (2d array)
     global answer_table
     global game_table
+    global row_col
 
     answer_list = image_names.copy() + image_names.copy()
     random.shuffle(answer_list)
